c1f942acd4686e91b8c80c56ca132626ce464091
udoykumar <udoypal.kumar2002@gmail.com>
2026-07-21 20:02:29 +0600

    http request sent and json request sent

--- module06-node-rest-api/src/server.ts
+++ module06-node-rest-api/src/server.ts
@@ -7,9 +7,25 @@ import {
 
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    console.log(req);
+    // console.log(req.url);  /, /user,  /product
+    // console.log(req.method); get, post, delete, patch
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/" && method === "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "this is root route sdfgdfg" }));
+    } else if (url?.startsWith("/products")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ message: "this is product route" }));
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+
+      res.end(JSON.stringify({ message: "not found" }));
+    }
   },
 );
-server.listen(5000, () => {
-  console.log("server is running on port 5000");
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
 });
