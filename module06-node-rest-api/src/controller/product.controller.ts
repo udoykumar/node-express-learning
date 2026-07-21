9ba29d812102481b0d5388b5d53ebb987ef03c3d
udoykumar <udoypal.kumar2002@gmail.com>
2026-07-21 22:31:14 +0600

    get method and single product get method

--- module06-node-rest-api/src/controller/product.controller.ts
+++ module06-node-rest-api/src/controller/product.controller.ts
@@ -1,5 +1,6 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { readProduct } from "../services/product.service";
+import type { IProduct } from "../types/product.type";
 
 export const productController = (
   req: IncomingMessage,
@@ -7,6 +8,10 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  console.log(id);
 
   if (url === "/products" && method === "GET") {
     // const products = [
@@ -20,5 +25,18 @@ export const productController = (
     res.end(
       JSON.stringify({ message: "this is product route", data: products }),
     );
+  } else if (method === "GET" && id !== null) {
+    // get single product
+
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "this is single product route",
+        data: product,
+      }),
+    );
+    console.log(product);
   }
 };
